4a0a03e6fdcb74b071b2edd1abe2ddfecac18943
Amandine Doug <amandine.douguedroit@gmail.com>
2021-02-01 16:48:05 +0100

    C1.4 Application - Jamais 2 sans 3

--- src/appli/Application.java
+++ src/appli/Application.java
@@ -4,5 +4,6 @@ public class Application {
 	public static void main(String[] args) {
 		System.out.println("Deuxième round");
 		System.out.println("Deuxième fournée");
+		System.out.println("Jamais deux sans trois");
 	}
 }
